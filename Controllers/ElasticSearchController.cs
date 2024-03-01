d2adc23227f7d44fad4b63a8306837970f313de3
Besir Gunduz <besirgunduz1993@gmail.com>
2024-03-01 16:41:28 +0300

    Add project files.

---
+++ Controllers/ElasticSearchController.cs
@@ -0,0 +1,85 @@
+﻿using BilginWithElasticSearch.Context;
+using Elasticsearch.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace BilginWithElasticSearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElasticSearchController : ControllerBase
+    {
+        AppDbContext context = new();
+
+        /// <summary>
+        /// ElasticSearch'e bağlanma ve kayıtların yüklenmesi
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        public async Task<IActionResult> SyncToElastic()
+        {
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200"));
+
+            var client = new ElasticLowLevelClient(settings);
+
+            List<Bilgin> bilginList = await context.Bilgin.ToListAsync();
+
+            var tasks = new List<Task>();
+
+            foreach (var bilgin in bilginList)
+            {
+                var response = await client.GetAsync<StringResponse>("bilgin", bilgin.Id.ToString());
+
+                if (response.HttpStatusCode != 200)
+                {
+                    tasks.Add(client.IndexAsync<StringResponse>("bilgin", bilgin.Id.ToString(), PostData.Serializable(bilgin)));
+                }
+            }
+
+            //DB'deki mevcut kayıtların tamamını indexlenmiş halde elasticSerarch'e kaydeder
+            await Task.WhenAll(tasks);
+
+            return Ok();
+        }
+
+
+        /// <summary>
+        /// ElasticSearch'te arama yapar.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpGet("[action]/{value}")]
+        public async Task<IActionResult> GetDataListWithElasticSearch(string value)
+        {
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200"));
+
+            var client = new ElasticLowLevelClient(settings);
+
+            var response = await client.SearchAsync<StringResponse>("bilgin", PostData.Serializable(new
+            {
+                query = new
+                {
+                    wildcard = new
+                    {
+                        Description = new { value = $"*{value}*" }
+                    }
+                }
+            })); 
+
+            var results = JObject.Parse(response.Body);
+
+            var hits = results["hits"]["hits"].ToObject<List<JObject>>();
+
+            List<Bilgin> bilginList = new();
+
+            foreach (var hit in hits)
+            {
+                bilginList.Add(hit["_source"].ToObject<Bilgin>());
+            }
+
+            return Ok(bilginList.Take(10));
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using BilginWithElasticSearch.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BilginWithElasticSearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        AppDbContext context = new();
+
+        /// <summary>
+        /// DB'ye 50.000 random kayıt atar
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        public async Task<IActionResult> CreateData(CancellationToken cancellationToken)
+        {
+            IList<Bilgin> travels = new List<Bilgin>();
+            var random = new Random();
+
+            for (int i = 0; i < 50000; i++)
+            {
+                //başlık 1 kelime
+                var title = new string(Enumerable.Repeat("abcdefgğhıijklmnoöprsştuwyz", 5)
+                    .Select(s => s[random.Next(s.Length)]).ToArray());
+
+                //içerik 500 kelime
+                var words = new List<string>();
+                for (int j = 0; j < 500; j++)
+                {
+                    words.Add(new string(Enumerable.Repeat("abcdefgğhıijklmnoöprsştuwyz", 5)
+                    .Select(s => s[random.Next(s.Length)]).ToArray()));
+                }
+
+                var description = string.Join(" ", words);
+                var bilgin = new Bilgin()
+                {
+                    Title = title,
+                    Description = description,
+                };
+
+                travels.Add(bilgin);
+            }
+
+            await context.Set<Bilgin>().AddRangeAsync(travels, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// DB'te arama yapar.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpGet("[action]/{value}")]
+        public async Task<IActionResult> GetDataListWithEF(string value)
+        {
+            IList<Bilgin> travels =
+                await context.Set<Bilgin>()
+                .Where(p => p.Description.Contains(value))
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(travels.Take(10));
+        }
+    }
+}
